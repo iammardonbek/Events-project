cd474e321236f2cbc434c31b72c2782538ea4bf5
iammardonbek <100706341+iammardonbek@users.noreply.github.com>
2022-09-14 18:37:21 +0500

    wef

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,30 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>no event found</p>;
+  }
+  console.log(event.address);
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        imageAlt={event.title}
+        address={event.location}
+        image={event.image}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
